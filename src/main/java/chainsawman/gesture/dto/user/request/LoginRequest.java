38b6dd6c89ff686b5886e55d447eb0f9f4de996c
yoonjeonggg <88yfg@naver.com>
2026-04-15 17:24:54 +0900

    login_request 수정

--- src/main/java/chainsawman/gesture/dto/user/request/LoginRequest.java
+++ src/main/java/chainsawman/gesture/dto/user/request/LoginRequest.java
@@ -3,8 +3,10 @@ package chainsawman.gesture.dto.user.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
 
