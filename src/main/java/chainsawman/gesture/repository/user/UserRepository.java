ca1e72f07b072d88525c8f849a08f723c5854cd1
yoonjeonggg <88yfg@naver.com>
2026-04-15 16:37:03 +0900

    repository 수정

--- src/main/java/chainsawman/gesture/repository/user/UserRepository.java
+++ src/main/java/chainsawman/gesture/repository/user/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmailAndIsDeactivatedFalse(String email);
+    Optional<User> findByEmailAndDeactivatedFalse(String email);
     boolean existsByEmail(String email);
     boolean existsById(String id);
 }
