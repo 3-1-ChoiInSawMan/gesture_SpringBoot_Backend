1a90b3d6a945fa36c1f34d345de76951056b0edf
yoonjeonggg <88yfg@naver.com>
2026-04-15 16:51:10 +0900

    isDeactivated 수정

--- src/main/java/chainsawman/gesture/repository/user/UserRepository.java
+++ src/main/java/chainsawman/gesture/repository/user/UserRepository.java
@@ -2,11 +2,15 @@ package chainsawman.gesture.repository.user;
 
 import chainsawman.gesture.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmailAndDeactivatedFalse(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeactivated = false")
+    Optional<User> findByEmailAndIsDeactivatedFalse(@Param("email") String email);
     boolean existsByEmail(String email);
     boolean existsById(String id);
 }
