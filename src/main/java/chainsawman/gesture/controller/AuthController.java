ca672d60f5b88c273a7684ddd6e06beecbd1d1ab
yoonjeonggg <88yfg@naver.com>
2026-04-17 10:43:21 +0900

    리프레시 토큰 검증&삭제 구현

--- src/main/java/chainsawman/gesture/controller/AuthController.java
+++ src/main/java/chainsawman/gesture/controller/AuthController.java
@@ -1,8 +1,10 @@
 package chainsawman.gesture.controller;
 
 import chainsawman.gesture.dto.user.request.LoginRequest;
+import chainsawman.gesture.dto.user.request.RefreshTokenRequest;
 import chainsawman.gesture.dto.user.request.RegisterRequest;
 import chainsawman.gesture.dto.user.response.LoginResponse;
+import chainsawman.gesture.dto.user.response.RefreshTokenValidationResponse;
 import chainsawman.gesture.dto.user.response.RegisterResponse;
 import chainsawman.gesture.global.ApiResponse;
 import chainsawman.gesture.service.AuthService;
@@ -10,10 +12,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -35,6 +34,21 @@ public class AuthController {
     public ResponseEntity<ApiResponse<RegisterResponse>> register(@RequestBody RegisterRequest request) {
         RegisterResponse result = authService.register(request);
         return ResponseEntity.ok(ApiResponse.ok(result, "요청이 성공적으로 처리되었습니다."));
+    }
+
+    @PostMapping("/refresh")
+    @Operation(summary = "리프레시 토큰 검증", description = "자동 로그인 시 사용하는 API 입니다.")
+    public ResponseEntity<ApiResponse<RefreshTokenValidationResponse>> refreshTokenValidation
+            (@RequestBody RefreshTokenRequest request) {
+        RefreshTokenValidationResponse result = authService.refreshTokenValidation(request);
+        return ResponseEntity.ok(ApiResponse.ok(result, "검증되었습니다."));
+    }
+
+    @DeleteMapping("/refresh")
+    @Operation(summary = "리프레시 토큰 삭제", description = "로그아웃 시 사용하는 API 입니다.")
+    public ResponseEntity<ApiResponse<Void>> deleteRefreshToken(@RequestBody RefreshTokenRequest request) {
+        authService.deleteRefreshToken(request);
+        return ResponseEntity.ok(ApiResponse.ok("삭제되었습니다."));
     }
 
 
